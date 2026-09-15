ce0cd1c8c5c075809a384bd2c7c85bafd9dc2b64
M.H.Jim <m.h.jim2357@gmail.com>
2026-09-15 12:41:08 +0600

    Configure express API for vercel

--- api/index.js
+++ api/index.js
@@ -1,24 +1,3 @@
-const express = require("express");
-const cors = require("cors");
-const pool = require("../server/db");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Psychology API is running!");
-});
-
-app.get("/test-db", async (req, res) => {
-    try {
-        const result = await pool.query("SELECT NOW()");
-        res.json(result.rows[0]);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Database connection failed");
-    }
-});
+const app = require("../server/server");
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -189,6 +189,14 @@ app.post("/participants", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
-});
+// app.listen(3000, () => {
+//     console.log("Server running on http://localhost:3000");
+// });
+
+if (require.main === module) {
+    app.listen(3000, () => {
+        console.log("Server running on http://localhost:3000");
+    });
+}
+
+module.exports = app;
