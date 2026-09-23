474823422d7fc87c18d277ae81edcb3b1666fcf2
M.H.Jim <m.h.jim2357@gmail.com>
2026-09-23 11:02:31 +0600

    add 4 new column for mcq

--- server/server.js
+++ server/server.js
@@ -34,7 +34,10 @@ app.post("/participants", async (req, res) => {
             correct_presses,
             false_presses,
             missed_sevens,
-            summary,
+            mcq1,
+            mcq2,
+            mcq3,
+            mcq4,
             post_corsi_score, post_corsi_percentile,
             post_trail_a,
             post_trail_b,
@@ -69,7 +72,10 @@ app.post("/participants", async (req, res) => {
             correct_presses, 
             false_presses, 
             missed_sevens, 
-            summary,
+            mcq1,
+            mcq2,
+            mcq3,
+            mcq4,
             post_corsi_score, post_corsi_percentile,
             post_trail_a,
             post_trail_b,
@@ -96,7 +102,7 @@ app.post("/participants", async (req, res) => {
                 $22, $23, $24, $25, $26,
                 $27, $28, $29, $30, $31,
 
-                $32, $33
+                $32, $33, $34, $35, $36
             )
             RETURNING *`,
             [
@@ -109,7 +115,10 @@ app.post("/participants", async (req, res) => {
                 correct_presses,
                 false_presses,
                 missed_sevens,
-                summary,
+                mcq1,
+                mcq2,
+                mcq3,
+                mcq4,
                 post_corsi_score, post_corsi_percentile,
                 post_trail_a,
                 post_trail_b,
